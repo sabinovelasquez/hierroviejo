bd7e1746d3f6c5b2e1dc5696918f4aff0e548604
Renato <renato@MacBook-Pro-de-Renato.local>
2021-01-15 00:54:04 -0300

    header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  header.header-box(style="border: 2px solid blue;")
+    .container(style="border: 2px solid red;")
+      .row.mb-5(style="border: 2px solid green;")
+        .col-12.d-flex.justify-content-center.align-content-center
+          h1.mt-5.mb-5 Hierro Viejo
+      .row
+        .col-12.col-lg-6
+          p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        .col-12.col-lg-6
+         p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .header-box{
+  width: 100%;
+  min-height: 100vh;
+  padding-top: 10px;
+}
+</style>
--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -27,7 +27,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'NavComponent',
   props: {
     msg: String
   }
@@ -36,18 +36,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
