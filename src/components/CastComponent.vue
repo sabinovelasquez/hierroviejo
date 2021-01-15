8dd7933f07f54144b7be4d556c4ff38a7d935110
Renato <renato@MacBook-Pro-de-Renato.local>
2021-01-15 06:00:05 -0300

    Ajuste Reparto

--- src/components/CastComponent.vue
+++ src/components/CastComponent.vue
@@ -1,28 +1,49 @@
 <template lang="pug">
-  section(style="border: 2px solid purple;")
+  section.cast-box(style="border: 2px solid purple;")
     .container-fluid.d-flex.flex-column.align-items-center(style="border: 2px solid red;")
       .container(style="border: 2px solid black;")
         .row(style="border: 2px solid fucsia;")
           .col-12.d-flex.justify-content-center
             h1.mb-5.mt-5 Reparto
-      .row
-        div(style="border: 2px solid green;").d-flex.justify-content-center.flex-column.flex-lg-row
-          b-card.mb-2.mr-3.text-center.hv-border-radius-card(title='Sabino Velazquez' img-src='https://picsum.photos/150/150/?image=25' img-class='rounded-circle' img-alt='Image' img-top='' tag='article' style='max-width: 20rem;')
-            b-card-text
-              | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-            b-button(href='#' variant='primary') Go somewhere
-          b-card.mb-2.mr-3.text-center.hv-border-radius-card(title='Alex' img-src='https://picsum.photos/150/150/?image=25' img-alt='Image' img-top='' tag='article' style='max-width: 20rem;')
-            b-card-text
-              | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-            b-button(href='#' variant='primary') Go somewhere
-          b-card.mb-2.mr-3.text-center.hv-border-radius-card(title='Renato Gonzalez' img-src='https://picsum.photos/150/150/?image=25' img-alt='Image' img-top='' tag='article' style='max-width: 20rem;')
-            b-card-text.text-center
-              | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-            b-button(href='#' variant='primary') Go somewhere
-          b-card.mb-2.mr-3.text-center.hv-border-radius-card(title='Card Title' img-src='https://picsum.photos/150/150/?image=25' img-alt='Image' img-top='' tag='article' style='max-width: 20rem;')
-            b-card-text.text-center
-              | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-            b-button(href='#' variant='primary') Go somewhere
+      .container-fluid
+        .row
+          .col-12.d-flex.flex-column.flex-lg-row.align-items-center.justify-content-lg-around(style="border: 2px solid green;")
+            b-card.mb-2.mr-3.text-center(tag='article' style='max-width: 13rem;')
+              b-avatar.mb-4(variant='info' src='https://placekitten.com/300/300' size="7rem")
+              b-card-title Alfred Hitchcock
+              b-card-text.text-center
+                | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
+              b-button(href='#' variant='primary') Go somewhere
+            b-card.mb-2.text-center(tag='article' style='max-width: 13rem;')
+              b-avatar.mb-4(variant='info' src='https://placekitten.com/300/300' size="7rem")
+              b-card-title Sam Reimi
+              b-card-text.text-center
+                | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
+              b-button(href='#' variant='primary') Go somewhere
+            b-card.mb-2.text-center(tag='article' style='max-width: 13rem;')
+              b-avatar.mb-4(variant='info' src='https://placekitten.com/300/300' size="7rem")
+              b-card-title David Fincher
+              b-card-text.text-center
+                | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
+              b-button(href='#' variant='primary') Go somewhere
+            b-card.mb-2.text-center(tag='article' style='max-width: 13rem;')
+              b-avatar.mb-4(variant='info' src='https://placekitten.com/300/300' size="7rem")
+              b-card-title James Microphone
+              b-card-text.text-center
+                | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
+              b-button(href='#' variant='primary') Go somewhere
+            b-card.mb-2.text-center(tag='article' style='max-width: 13rem;')
+              b-avatar.mb-4(variant='info' src='https://placekitten.com/300/300' size="7rem")
+              b-card-title Tim burton
+              b-card-text.text-center
+                | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
+              b-button(href='#' variant='primary') Go somewhere
+            b-card.mb-2.text-center(tag='article' style='max-width: 13rem;')
+              b-avatar.mb-4(variant='info' src='https://placekitten.com/300/300' size="7rem")
+              b-card-title Quentin Tarantino
+              b-card-text.text-center
+                | Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
+              b-button(href='#' variant='primary') Go somewhere
 </template>
 
 <script>
@@ -32,6 +53,7 @@ export default {
     msg: String
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -41,5 +63,10 @@ export default {
       border-radius:100px;
       margin-bottom: 30px;
     }
+  }
+.cast-box{
+  width: 100%;
+  min-height: 100vh;
+  padding-top: 10px;
   }
 </style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  h1.text-center Footer de Hierro Viejo
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
