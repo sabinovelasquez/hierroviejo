e4ac18cb83f8cb6a451aa06453381c1ae95c8760
Renato <renato@MacBook-Pro-de-Renato.local>
2021-01-14 23:22:42 -0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <template lang="pug">
-  #app
-    img(alt="Vue logo", src="./assets/logo.png")
     HelloWorld
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,28 @@
 <template lang="pug">
-  section
-    h1 XOROOO
-    .container
-      button.btn.btn-primary(type="button") NAKOO
+  div
+    b-navbar(toggleable='lg' type='dark' variant='info')
+      b-navbar-brand(href='#') NavBar
+      b-navbar-toggle(target='nav-collapse')
+      b-collapse#nav-collapse(is-nav='')
+        b-navbar-nav
+          b-nav-item(href='#') Link
+          b-nav-item(href='#' disabled='') Disabled
+        // Right aligned nav items
+        b-navbar-nav.ml-auto
+          b-nav-form
+            b-form-input.mr-sm-2(size='sm' placeholder='Search')
+            b-button.my-2.my-sm-0(size='sm' type='submit') Search
+          b-nav-item-dropdown(text='Lang' right='')
+            b-dropdown-item(href='#') EN
+            b-dropdown-item(href='#') ES
+            b-dropdown-item(href='#') RU
+            b-dropdown-item(href='#') FA
+          b-nav-item-dropdown(right='')
+            // Using 'button-content' slot
+            template(#button-content='')
+              em User
+            b-dropdown-item(href='#') Profile
+            b-dropdown-item(href='#') Sign Out
 </template>
 
 <script>
